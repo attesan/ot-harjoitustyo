1e65d47a887a340760645a7ef6e9930fdcb180af
Atte Sanasvuori <atte.sanasvuori@helsinki.fi>
2023-04-04 23:12:04 +0300

    config pohja luotu

--- src/config.py
+++ src/config.py
@@ -1,3 +1,11 @@
 import os
 from dotenv import load_dotenv
 
+directory_name = os.path.dirname(__file__)
+
+try:
+    load_dotenv(dotenv_path=os.path.join(directory_name, "..", ".env"))
+except FileNotFoundError:
+    pass
+
+DEVICES_FILENAME = os.getenv("DEVICES_FILENAME") or "Devices.dp"
