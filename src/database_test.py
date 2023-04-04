0c4deb46d1b137764e7e85a3a708046d8e9ba6b2
Atte Sanasvuori <atte.sanasvuori@helsinki.fi>
2023-04-04 23:41:19 +0300

    lisätty pytest ja testipohja device_database

---
+++ src/database_test.py
@@ -0,0 +1,9 @@
+import unittest
+from repository.device_repository import DeviceRepository
+
+class TestDeviceDatabase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_create(self):
+        pass
