d52f0184e74e5c8b905ba3e36cfa1650c8b1e275
Atte Sanasvuori <atte.sanasvuori@helsinki.fi>
2023-05-14 23:53:55 +0300

    testejä

---
+++ src/tests/project_device_maker_test.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+import sys
+from services.project_device_maker import ProjectDeviceMaker
+from repository.device_repository import DeviceRepository
+from initialize_database import initialize_database
+from services.project_data_service import ProjectDataService
+from entities.project_device import ProjectDevice
+
+sys.path.insert(0, os.path.abspath(".."))
+
+class TestProjectDeviceMaker(unittest.TestCase):
+    def setUp(self):
+        self.d = ProjectDeviceMaker()
+        self.device = ("model1", "manufacturer1",[("point11","text11","type11"),("point12","text12","type12"),("point13","text13","type13"),("point14","text14","type14")])
+        self.d._devices.new_device(self.device[0],self.device[1],self.device[2])
+
+    def test_has_device_repository(self):
+        self.assertEqual(self.d._devices.__class__,DeviceRepository().__class__)
